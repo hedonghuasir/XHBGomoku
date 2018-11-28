1582b41992dd12b40a201f7fa786c47a256ed733
hedonghua <hedonghuasir@163.com>
2018-11-28 13:51:27 +0800

    upload_2

--- XHBGomoku/XHBAppDelegate.h
+++ XHBGomoku/XHBAppDelegate.h
@@ -7,7 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
-static NSString *appKey = @"5717ca216213bcefaf6490ea";
+static NSString *appKey = @"66cc47fa08157cb4628bf71e";
 static NSString *channel = @"App Store";
 static BOOL isProduction = YES;
 @interface XHBAppDelegate : UIResponder <UIApplicationDelegate>
